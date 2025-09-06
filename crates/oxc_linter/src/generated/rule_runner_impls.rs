202961a3cd28446c3b7b5cb4b6d366728959b1ef
Sysix <sysix@sysix-coding.de>
2025-09-06 16:20:19 +0200

    feat(linter): add `vue/define-emits-declaration` rule

--- crates/oxc_linter/src/generated/rule_runner_impls.rs
+++ crates/oxc_linter/src/generated/rule_runner_impls.rs
@@ -2982,6 +2982,11 @@ impl RuleRunner for crate::rules::vitest::require_local_test_context_for_concurr
 
 }
 
+impl RuleRunner for crate::rules::vue::define_emits_declaration::DefineEmitsDeclaration {
+    const NODE_TYPES: &AstTypesBitset = &AstTypesBitset::new();
+    const ANY_NODE_TYPE: bool = true;
+}
+
 impl RuleRunner for crate::rules::vue::define_props_declaration::DefinePropsDeclaration {
     const NODE_TYPES: &AstTypesBitset = &AstTypesBitset::new();
     const ANY_NODE_TYPE: bool = true;
